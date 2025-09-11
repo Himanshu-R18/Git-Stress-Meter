f6c139a395538a5329d934b4bf2af45cbf4c9d96
Himanshu-R18 <bobyrawat004@gmail.com>
2025-09-11 07:12:33 +0530

    about page finalised

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from "react";
 const About = () => {
   return (
-    <div>About page</div>
-  )
-}
+    <div className="about-container">
+      <div className="about-top">
+        <div className="vision-img">
+          <img src="/vision1.jpg" alt="Our Vision" id="vision"/>
+        </div>
+        <div className="vision-title">
+          <div className="vision-head">
+            <h1>Vision</h1>
+          </div>
+          <div className="vision-content">
+            <p>“To create a healthier and happier society by helping individuals manage their mood and stress effectively through technology-driven solutions.”</p>
+          </div>
+        </div>
+      </div>
+      <div className="about-bottom">
+        <div className="mission-img">
+          <img src="/mission2.jpg" alt="Our Mission" id="mission"/>
+        </div>
+        <div className="mission-title">
+          <ul className="about-list">
+            <h1>Mission</h1>
+            <li>To provide users with an easy-to-use platform that tracks mood and stress levels.</li>
+            <li>To suggest personalized exercises, relaxation tips, and activities for stress relief.</li>
+            <li>To promote mental well-being by making stress management accessible to everyone.</li>
+            <li>To encourage healthy lifestyle habits and reduce the risk of stress-related health issues.</li>
+          </ul>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/QuestionBank.jsx
+++ src/QuestionBank.jsx
@@ -11,23 +11,23 @@ const QuestionBank = (props) => {
 
           <div className="ans" id="ans1">
             <input type="radio" name={"answer"+props.sl} id="anss1" />
-            <label htmlFor="anss1"> 😭 Very Bad</label>
+            <label htmlFor="anss1"> Very Bad 😭 </label>
           </div>
           <div className="ans" id="ans2">
             <input type="radio" name={"answer"+props.sl} id="anss2" />
-            <label htmlFor="anss2"> ☹️ Bad</label>
+            <label htmlFor="anss2"> Bad ☹️ </label>
           </div>
           <div className="ans" id="ans3">
             <input type="radio" name={"answer"+props.sl} id="anss3" />
-            <label htmlFor="anss3"> 🙂 Average</label>
+            <label htmlFor="anss3"> Average 🙂 </label>
           </div>
           <div className="ans" id="ans4">
             <input type="radio" name={"answer"+props.sl} id="anss4" />
-            <label htmlFor="anss4"> 😊 Good</label>
+            <label htmlFor="anss4"> Good 😊 </label>
           </div>
           <div className="ans" id="ans5">
             <input type="radio" name={"answer"+props.sl} id="anss5" />
-            <label htmlFor="anss5"> 😁 Very Good</label>
+            <label htmlFor="anss5"> Very Good 😁 </label>
           </div>
         </div>
       </div>
